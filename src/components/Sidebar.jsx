6c3d3bf175551afb38547cbaaddb64a900392057
DCJ <jekson998@blaster.co.id>
2025-07-23 11:22:22 +0700

    Checkpoint

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,106 +1,38 @@
 export default function SideBar({ projectList, onCreate, className }) {
   const idleBar =
-    "w-full py-1 px-2 my-1 rounded-md font-[nunito-sans] text-left text-white transition hover:bg-green-600 active:bg-green-500";
+    "w-full py-1 px-2 mt-1 rounded-md font-[nunito-sans] text-left text-white transition hover:bg-green-600 active:bg-green-500";
   const activeBar = `${idleBar} bg-green-800`;
-  const shadowStyle =
-    "absolute w-[80%] h-5 bg-green-500 shadow-md shadow-green-950";
+  const shadowIndicator = "w-[18vw] h-4 bg-green-950/80 absolute";
 
   return (
     <div className={className}>
-      <div className="pt-10">
+      <div className="pt-10 pb-4">
         <h1 className="font-[nunito-sans] uppercase text-gray-100 text-xl font-bold">
           Your projects
         </h1>
         <button
           type="button"
-          onClick={() => onCreate()}
           className="btn-primary mt-6"
+          onClick={() => onCreate()}
         >
           + Add Project
         </button>
       </div>
 
-      <br />
-      <br />
+      <div className="max-h-[67.5vh] pr-2 pb-8 overflow-y-auto no-scrollbar">
+        {/* This is a shadow and gap */}
+        <div className={`${shadowIndicator} shadow-bottom`}></div>
+        <div className="my-5"></div>
 
-      {/* <div className={shadowStyle}></div> */}
-
-      <div className="flex flex-col max-h-[60vh] overflow-y-scroll no-scrollbar">
         {projectList.length > 0 &&
           projectList.map((project, index) => (
-            <div>
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={idleBar}>
-                {project.title}Hello
-              </button>
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>{" "}
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>
-              <button key={index} type="button" className={activeBar}>
-                {project.title}Hello
-              </button>
-            </div>
+            <button key={index} type="button" className={idleBar}>
+              {project.title}Hello
+            </button>
           ))}
-      </div>
 
-      {/* <div className={shadowStyle}></div> */}
-      {/* <br /> */}
+        <div className={`${shadowIndicator} shadow-top bottom-0`}></div>
+      </div>
     </div>
   );
 }
