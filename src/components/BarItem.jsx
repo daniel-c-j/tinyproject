33497fdb98c41abd92403710d1c7244a750fcf76
DCJ <jekson998@blaster.co.id>
2025-07-26 01:03:01 +0700

    Fix overflow bar item

---
+++ src/components/BarItem.jsx
@@ -0,0 +1,44 @@
+function optimizeTitle(title, length) {
+  const words = title.split(" ");
+  let tempTitle = "";
+  for (const word of words) {
+    if (word.length > length) {
+      tempTitle += " " + word.slice(0, length) + "...";
+      break;
+    }
+    tempTitle += " " + word;
+  }
+  return tempTitle;
+}
+
+export default function BarItem({ item, project, onSelect }) {
+  const barStyle = item === project ? "bar-item-active" : "bar-item-idle";
+
+  return (
+    <button
+      type="button"
+      className={barStyle + " "}
+      onClick={() => onSelect(project)}
+    >
+      <span className="sm:hidden inline font-bold">
+        {project.title.slice(0, 2)}
+      </span>
+
+      <span className="hidden sm:inline md:hidden">
+        {optimizeTitle(project.title, 12)}
+      </span>
+
+      <span className="hidden md:inline lg:hidden">
+        {optimizeTitle(project.title, 16)}
+      </span>
+
+      <span className="hidden lg:inline xl:hidden">
+        {optimizeTitle(project.title, 22)}
+      </span>
+
+      <span className="hidden xl:inline">
+        {optimizeTitle(project.title, 26)}
+      </span>
+    </button>
+  );
+}
