a0a4a4c5be5630e65deb1114614c8e8de7eb851c
DCJ <jekson998@blaster.co.id>
2025-08-08 12:49:45 +0700

    Fix config base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // base: '/tinyproject/',
+  base: '/tinyproject/',
   plugins: [react(), tailwindcss()],
 })
